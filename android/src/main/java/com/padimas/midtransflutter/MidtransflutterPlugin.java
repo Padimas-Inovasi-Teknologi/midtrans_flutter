0284534714d3087bda87c6bd4be4d4f9bec12684
nemob-dev <reno.rizky.ollantino@gmail.com>
2020-08-25 15:47:48 +0700

    Fix Plugin

--- android/src/main/java/com/padimas/midtransflutter/MidtransflutterPlugin.java
+++ android/src/main/java/com/padimas/midtransflutter/MidtransflutterPlugin.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 
 import androidx.annotation.NonNull;
 
-import com.midtrans.sdk.corekit.BuildConfig;
+
 import com.midtrans.sdk.corekit.callback.CardTokenCallback;
 import com.midtrans.sdk.corekit.core.MidtransSDK;
 import com.midtrans.sdk.corekit.core.SdkCoreFlowBuilder;
@@ -14,6 +14,8 @@ import com.midtrans.sdk.corekit.models.CardTokenRequest;
 import com.midtrans.sdk.corekit.models.TokenDetailsResponse;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -24,10 +26,9 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 import static android.app.Activity.RESULT_OK;
 
 /** MidtransflutterPlugin */
-public class MidtransflutterPlugin implements FlutterPlugin, MethodCallHandler, PluginRegistry.ActivityResultListener {
+public class MidtransflutterPlugin implements FlutterPlugin, MethodCallHandler {
 
   private Context context;
-  private Activity activity;
   public static final int REQUEST_RENT_FEE = 4569;
   private static String token;
   private static Result result;
@@ -35,7 +36,15 @@ public class MidtransflutterPlugin implements FlutterPlugin, MethodCallHandler, 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "midtransflutter");
-    channel.setMethodCallHandler(new MidtransflutterPlugin());
+
+    MidtransflutterPlugin plugin = new MidtransflutterPlugin();
+    channel.setMethodCallHandler(plugin);
+    plugin.context = flutterPluginBinding.getApplicationContext();
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+
   }
 
   public static void registerWith(Registrar registrar) {
@@ -43,11 +52,7 @@ public class MidtransflutterPlugin implements FlutterPlugin, MethodCallHandler, 
     channel.setMethodCallHandler(new MidtransflutterPlugin());
     MidtransflutterPlugin plugin = new MidtransflutterPlugin();
     channel.setMethodCallHandler(plugin);
-
     plugin.context = registrar.context();
-    plugin.activity = registrar.activity();
-
-    registrar.addActivityResultListener(plugin);
   }
 
   @Override
@@ -79,9 +84,9 @@ public class MidtransflutterPlugin implements FlutterPlugin, MethodCallHandler, 
   private void configure(String clientKey, boolean isProduction) {
     String merchantBaseUrl;
     if (!isProduction) {
-      merchantBaseUrl = "https://api.sandbox.veritrans.co.id/v2/transactions";
+      merchantBaseUrl = "https://api.sandbox.veritrans.co.id/v2/transactions/";
     } else {
-      merchantBaseUrl = "https://api.veritrans.co.id/v2/transactions";
+      merchantBaseUrl = "https://api.veritrans.co.id/v2/transactions/";
     }
 
     SdkCoreFlowBuilder.init()
@@ -113,11 +118,12 @@ public class MidtransflutterPlugin implements FlutterPlugin, MethodCallHandler, 
       public void onSuccess(TokenDetailsResponse tokenDetailsResponse) {
         String token = tokenDetailsResponse.getTokenId();
         String url = tokenDetailsResponse.getRedirectUrl();
+        if (result != null) result.success("Token: " + (token == null ? "" : token));
 
-        MidtransflutterPlugin.this.token = token;
-        Intent intent = new Intent(activity, WebviewVerifyActivity.class);
-        intent.putExtra(WebviewVerifyActivity.EXTRA_URL, url);
-        activity.startActivityForResult(intent, REQUEST_RENT_FEE);
+//        MidtransflutterPlugin.this.token = token;
+//        Intent intent = new Intent(activity, WebviewVerifyActivity.class);
+//        intent.putExtra(WebviewVerifyActivity.EXTRA_URL, url);
+//        activity.startActivityForResult(intent, REQUEST_RENT_FEE);
       }
 
       @Override
@@ -130,29 +136,5 @@ public class MidtransflutterPlugin implements FlutterPlugin, MethodCallHandler, 
         result.error("Error: " + throwable.getMessage(), "", "");
       }
     });
-  }
-
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-  }
-
-  @Override
-  public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-    return triggerResult(requestCode, resultCode, data);
-  }
-
-  public static boolean triggerResult(int requestCode, int resultCode, Intent intent) {
-    if (requestCode == REQUEST_RENT_FEE) {
-      if (resultCode == RESULT_OK) {
-        if (result != null) result.success("Token: " + (token == null ? "" : token));
-      } else {
-        String cancelMessage = "3D Secure transaction canceled by user";
-        String message = intent == null ? cancelMessage : intent.getStringExtra("message");
-        result.error("Error", message, "");
-      }
-    } else {
-      result.success("Failed!");
-    }
-    return false;
   }
 }
